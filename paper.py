3997fa6d2aaee91970d7cfae8a0d67af034c957f
Henry Ji <jeep.jipu@gmail.com>
2024-05-06 13:50:43 +0800

    add test_on_averageDegrees

--- paper.py
+++ paper.py
@@ -1,13 +1,14 @@
 from matplotlib import pyplot as plt
-
+from networkx import Graph, barabasi_albert_graph
 from utils.toolbox import GraphTool
 from algorithm_collection import *
 from algorithm_comparation import *
 from env import *
 from data_processing import DatasetLoader
+from algorithm_collection import _Algorithm
 
 
-def get_algorithms():
+def get_algorithms() -> list[_Algorithm]:
     algorithms = [
         get_proposed_algorithm(),
         HDAAlgorithm(),
@@ -50,9 +51,27 @@ def get_firstGraphInfo_from_dataset(datasetName: str):
     print(f"{datasetName}: {GraphTool.summarize_graph(graph)}")
 
 
+def test_on_averageDegrees(
+    graphNum: int = 1,
+    graphNodeNum: int = 500,
+    averageDegreeList: list[int] = [2, 5, 10, 20, 50, 100],
+):
+    algorithms = get_algorithms()
+    print(f"节点数: {graphNodeNum}")
+    for averageDegree in averageDegreeList:
+        print(f"averageDegree: {averageDegree}")
+        graphs: list[Graph] = [
+            barabasi_albert_graph(graphNodeNum, int(averageDegree / 2))
+            for _ in range(graphNum)
+        ]
+        result = compare_algorithms_on_graphs_by_anc(algorithms, graphs)
+        print(result)
+
+
 if __name__ == "__main__":
-    datasetName = "karateclub"
+    # datasetName = "karateclub"
     # plot_on_synthetic_datasets()
-    plot_on_real_dataset(datasetName)
+    # plot_on_real_dataset(datasetName)
     # calc_anc_on_dataset(datasetName)
     # get_firstGraphInfo_from_dataset(datasetName)
+    test_on_averageDegrees(graphNodeNum=500)
